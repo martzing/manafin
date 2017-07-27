5527f4aa4aad020111262ace2f5911101ff6d0bb
mart <martzing25@gmail.com>
2017-07-28 02:31:56 +0700

    change url and rename manafincalwebservice file

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://manafin.punyapat.org/web'),
+    'url' => env('APP_URL', 'http://manafin-web.punyapat.org/'),
 
     /*
     |--------------------------------------------------------------------------
